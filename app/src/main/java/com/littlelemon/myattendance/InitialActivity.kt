5a632e28e7f1d16bd7384e053798fb773667e0dd
Saifuddin <adenwala32@gmail.com>
2024-02-04 12:01:58 +0530

    InitialActivity created

---
+++ app/src/main/java/com/littlelemon/myattendance/InitialActivity.kt
@@ -0,0 +1,58 @@
+package com.littlelemon.myattendance
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun InitialActivity() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(36.dp)
+            ) {
+            Text(text = "Add Subjects",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(12.dp))
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(36.dp)
+        ) {
+            Text(text = "Edit Timetable ",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(12.dp))
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(36.dp)
+        ) {
+            Text(text = "Timetable",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(12.dp))
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(36.dp)
+        ) {
+            Text(text = "Status",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(12.dp))
+        }
+    }
+}
